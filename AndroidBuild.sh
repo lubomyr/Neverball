8dc8bdc277b294513dfa73e66c23f4fbcfabb608
lubomyr <lubomyr31@gmail.com>
2016-11-06 16:50:34 +0200

    glshim library renamed to gl4es

--- AndroidBuild.sh
+++ AndroidBuild.sh
@@ -6,7 +6,7 @@ LOCAL_PATH=`cd $LOCAL_PATH && pwd`
 
 JOBS=1
 
-ln -sf libglshim.a $LOCAL_PATH/../../../obj/local/$1/libGL.a
+ln -sf libgl4es.a $LOCAL_PATH/../../../obj/local/$1/libGL.a
 
 if [ "$1" = armeabi-v7a ]; then
 ../setEnvironment-armeabi-v7a.sh sh -c "cd neverball-1.5.4 && make" && mv -f neverball-1.5.4/neverball libapplication-armeabi-v7a.so
